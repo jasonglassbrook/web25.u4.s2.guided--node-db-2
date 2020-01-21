3c69a3a56c5cd1ddc96b2c0a3df50bc496860a96
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-21 13:34:51 -0500

    update code style

--- fruits/fruits-router.js
+++ fruits/fruits-router.js
@@ -1,46 +1,46 @@
-const express = require('express');
-const knex = require('knex');
-const dbConfig = require('../knexfile')
+const express = require ('express')
+const knex = require ('knex')
+const dbConfig = require ('../knexfile')
 
-const db = knex(dbConfig.development);
+const db = knex (dbConfig.development)
 
-const router = express.Router();
+const router = express.Router ()
 
-router.get('/', (req, res) => {
-  db('fruits')
-  .then(fruits => {
-    res.json(fruits);
+router.get ('/', (req, res) => {
+  db ('fruits')
+  .then (fruits => {
+      res.json (fruits)
   })
   .catch (err => {
-    res.status(500).json({ message: 'Failed to retrieve fruits' });
-  });
-});
+    res.status (500).json ({ message: 'Failed to retrieve fruits' })
+  })
+})
 
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
+router.get ('/:id', (req, res) => {
+  const { id } = req.params
 
-  db('fruits').where({ id }).first()
-  .then(fruit => {
-    res.json(fruit);
+  db ('fruits').where ({ id }).first ()
+  .then (fruit => {
+    res.json (fruit)
   })
   .catch (err => {
-    res.status(500).json({ message: 'Failed to retrieve fruit' });
-  });
-});
+    res.status (500).json ({ message: 'Failed to retrieve fruit' })
+  })
+})
 
-router.post('/', (req, res) => {
-  const fruitData = req.body;
-  db('fruits').insert(fruitData)
-  .then(ids => {
-    db('fruits').where({ id: ids[0] })
-    .then(newFruitEntry => {
-      res.status(201).json(newFruitEntry);
-    });
+router.post ('/', (req, res) => {
+  const fruitData = req.body
+  db ('fruits').insert (fruitData)
+  .then (ids => {
+    db ('fruits').where ({ id: ids[0] })
+    .then (newFruitEntry => {
+      res.status (201).json (newFruitEntry)
+    })
   })
   .catch (err => {
-    console.log('POST error', err);
-    res.status(500).json({ message: "Failed to store data" });
-  });
-});
+    console.log ('POST error', err)
+    res.status (500).json ({ message: "Failed to store data" })
+  })
+})
 
-module.exports = router;
+module.exports = router
