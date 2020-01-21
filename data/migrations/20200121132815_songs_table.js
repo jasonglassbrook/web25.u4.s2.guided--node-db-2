74667c4d66b27b0e052ac050d5c8e1b9a20094b3
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-21 13:49:23 -0500

    write migrations for songs_table

--- data/migrations/20200121132815_songs_table.js
+++ data/migrations/20200121132815_songs_table.js
@@ -1,6 +1,26 @@
 
 exports.up = function (knex) {
-
+  return knex.schema.createTable ('songs', (table) => {
+    table
+      .uuid ('Id')
+      .primary ()
+    table
+      .timestamps (true, true)
+    // table
+    //   .increments ('Id')
+    //   .primary ()
+    table
+      .string ('Name', 255)
+      .index ()
+    table
+      .string ('Artist', 255)
+      .index ()
+    table
+      .integer ('Duration')
+    table
+      .boolean ('Favorite')
+      .defaultTo (false)
+  })
 }
 
 exports.down = function (knex) {
